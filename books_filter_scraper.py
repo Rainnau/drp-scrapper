34006033643491640ba14a627de26d0f86b73fbf
Naunau <ahmadhanifnaufaljamil@gmail.com>
2025-11-13 16:21:39 +0700

    updated

--- books_filter_scraper.py
+++ books_filter_scraper.py
@@ -2,17 +2,38 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 import json
 import time
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+chrome_options = Options()
+chrome_options.add_argument("--log-level=3")
+driver = webdriver.Chrome(options=chrome_options, service=Service(ChromeDriverManager().install()))
 driver.get("http://books.toscrape.com/")
 time.sleep(2)
 
-print("=== Books Scraper ===")
-category_input = input("Masukkan kategori buku (misal: travel, music, poetry, art, dll): ").strip().lower()
-min_price = float(input("Masukkan harga minimum (contoh: 0): "))
-max_price = float(input("Masukkan harga maksimum (contoh: 100): "))
+category_input: str
+min_price: float
+max_price: float
+
+print("\n=== Books Scraper ===")
+
+while True:
+    try:
+        usr_input = input("Masukkan kategori buku (misal: travel, music, poetry, art, dll): ")
+        category_input = str(usr_input).strip().lower()
+
+        usr_input = input("Masukkan harga minimum (contoh: 0): ")
+        min_price = float(usr_input)
+        
+        usr_input = input("Masukkan harga maksimum (contoh: 100): ")
+        max_price = float(usr_input)
+    except ValueError:
+        print("\n~!!!~ Salah input ~!!!~\n")
+        continue
+    else:
+        print("\n=== Pencarian Buku ===")
+        break
 
 books_data = []
 
@@ -49,8 +70,10 @@ finally:
 if books_data:
     total_books = len(books_data)
     avg_price = sum(book["price"] for book in books_data) / total_books
-    print(f"\nTotal buku ditemukan: {total_books}")
+    print(f"Total buku ditemukan: {total_books}")
     print(f"Rata-rata harga: £{avg_price:.2f}")
+    print(f"Harga minimum: ")
+    print(f"Harga maximum: ")
     print("Data disimpan ke 'books_filtered.json'")
     with open("books_filtered.json", "w", encoding="utf-8") as f:
         json.dump(books_data, f, indent=4)
